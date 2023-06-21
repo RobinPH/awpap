c7e6b360f52623355b14f440fd159522af774e8c
maui30 <103097103+maui30@users.noreply.github.com>
2023-06-21 16:41:44 +0800

    Picture with information

---
+++ resources/views/livewire/info-card.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="p-8 lg:pl-14 lg:pb-14 lg:pt-14 lg:pr-4">
+        <div class="grid grid-cols-1 lg:grid-cols-7 justify-items-center">
+            <div class="col-span-3">
+                <img src="./assets/store.jpg" alt="" class="rounded-xl drop-shadow-xl">
+            </div>
+            <div class="flex flex-col justify-center p-10 text-justify col-span-4">
+                <h1 class="font-bold text-3xl pb-8">Animal Experimentation</h1>
+                <p class="text-lg"><b class="text-blue-500">Furfecto Veterinary Services</b> is a leading clinic
+                    providing exceptional care for pets. Their dedicated team of skilled veterinarians offers
+                    personalized attention and advanced medical expertise. From routine check-ups to specialized
+                    treatments, Furfecto Veterinary Services ensures the health and well-being of your furry
+                    friends. With state-of-the-art facilities and a focus on preventive medicine, they prioritize
+                    individualized care and client satisfaction. Trust Furfecto Veterinary Services for top-notch
+                    pet healthcare.</p>
+            </div>
+        </div>
+    </div>
+</div>
