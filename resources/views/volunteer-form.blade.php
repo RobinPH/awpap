e316d04295f624e0f37f3a462605c6ac74698140
maui30 <103097103+maui30@users.noreply.github.com>
2023-06-21 22:11:18 +0800

    updated forms

--- resources/views/volunteer-form.blade.php
+++ resources/views/volunteer-form.blade.php
@@ -1,33 +1,52 @@
 <x-layout>
     <h1 class="text-center font-bold text-3xl pt-10 pb-6">Fill Up Volunteer Form</h1>
+
     <div class="flex justify-center p-4">
         <div class="bg-white p-16 md:w-9/12 rounded-xl drop-shadow-xl border border-solid border-gray-200">
-            <h2 class="border-solid border-b-2 border-slate-950 font-bold text-2xl">Owner's Info</h2>
-            <p class="py-4">Name<b class="text-red-500">*</b></p>
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
-                <div class="">
-                    <input type="text" placeholder="First Name"
-                        class="w-full border-solid border-b-2 border-slate-400">
-                </div>
-                <div class="">
-                    <input type="text" placeholder="First Name"
-                        class="w-full border-solid border-b-2 border-slate-400">
-                </div>
-            </div>
+            <div>
+                <h2 class="border-solid border-b-2 border-slate-950 font-bold text-2xl">Applicant's Info</h2>
+                <p class="py-4">Name<b class="text-red-500">*</b></p>
 
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
-                <div>
-                    <p class="pt-8">Phone<b class="text-red-500">*</b></p>
-                    <input type="text" placeholder="First Name"
-                        class="w-full border-solid border-b-2 border-slate-400">
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+                    <div>
+                        <input type="text" placeholder="First Name"
+                            class="w-full border-solid border-b-2 border-slate-400">
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Last Name"
+                            class="w-full border-solid border-b-2 border-slate-400">
+                    </div>
                 </div>
-                <div>
-                    <p class="pt-8">Location<b class="text-red-500">*</b></p>
-                    <input type="text" placeholder="Last Name"
-                        class="w-full border-solid border-b-2 border-slate-400">
+
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+                    <div>
+                        <p class="pb-4 pt-8">Phone<b class="text-red-500">*</b></p>
+                        <input type="text" class="w-full border-solid border-b-2 border-slate-400">
+                    </div>
+                    <div>
+                        <p class="pb-4 lg:pt-8">Location<b class="text-red-500">*</b></p>
+                        <input type="text" class="w-full border-solid border-b-2 border-slate-400">
+                    </div>
+
+                </div>
+
+                <div class="pt-8 lg:px-32">
+                    <label for="volunteer" class="">Type of Volunteer Work?<b class="text-red-500">*</b></p>
+                        <select name="volunteer" id="volunteer" class="w-full border border-slate-400 rounded-lg">
+                            <option value="volvo">Animal Care</option>
+                            <option value="saab">Office/Admin work</option>
+                            <option value="mercedes">Clinic</option>
+                        </select>
+                </div>
+
+                <div class="pt-8 flex justify-center items-center">
+                    <button type="button"
+                        class="font-bold text-white hover:bg-gray-800 bg-slate-950 rounded-full text-sm px-5 py-2.5 mr-2 mb-2 ">
+                        Submit
+                    </button>
                 </div>
             </div>
         </div>
-    </div>
+
     </div>
 </x-layout>
