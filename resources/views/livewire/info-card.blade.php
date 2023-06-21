3b61f4402cb2fc431541693ddf575ced2a13a976
maui30 <103097103+maui30@users.noreply.github.com>
2023-06-21 17:10:44 +0800

    changed into variables

--- resources/views/livewire/info-card.blade.php
+++ resources/views/livewire/info-card.blade.php
@@ -5,14 +5,8 @@
                 <img src="./assets/store.jpg" alt="" class="rounded-xl drop-shadow-xl">
             </div>
             <div class="flex flex-col justify-center p-10 text-justify col-span-4">
-                <h1 class="font-bold text-3xl pb-8">Animal Experimentation</h1>
-                <p class="text-lg"><b class="text-blue-500">Furfecto Veterinary Services</b> is a leading clinic
-                    providing exceptional care for pets. Their dedicated team of skilled veterinarians offers
-                    personalized attention and advanced medical expertise. From routine check-ups to specialized
-                    treatments, Furfecto Veterinary Services ensures the health and well-being of your furry
-                    friends. With state-of-the-art facilities and a focus on preventive medicine, they prioritize
-                    individualized care and client satisfaction. Trust Furfecto Veterinary Services for top-notch
-                    pet healthcare.</p>
+                <h1 class="font-bold text-3xl pb-8">{{ $title }}</h1>
+                <p class="text-lg">{{ $description }}</p>
             </div>
         </div>
     </div>
