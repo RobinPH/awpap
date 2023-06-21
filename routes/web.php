c684a9d28dff625b175b58635ee07fbaa6cc2fb9
maui30 <103097103+maui30@users.noreply.github.com>
2023-06-21 16:45:14 +0800

    added additional routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,28 @@ Route::get('/', function () {
 Route::get('/animal', function () {
     return view('animal');
 });
+
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+
+Route::get('/program', function () {
+    return view('program');
+});
+
+Route::get('/guideline', function () {
+    return view('guideline');
+});
+
+Route::get('/join-us', function () {
+    return view('join-us');
+});
+
+Route::get('/sign-in', function () {
+    return view('sign-in');
+});
+
+Route::get('/sign-up', function () {
+    return view('sign-up');
+});
+
